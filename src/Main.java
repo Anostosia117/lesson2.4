c0c5b811104590850a367c0e1d8e77fdaefbd02a
Anostosia117 <jigsa@yandex.ru>
2022-06-03 23:19:54 +0300

    готово 3.0

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,51 @@
+import exeptions.WrongEnterException;
+import exeptions.WrongLenghtException;
 import exeptions.WrongLoginException;
 import exeptions.WrongPasswordException;
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
 
 public class Main {
     public static boolean check(String login, String password, String confirmPassword) throws WrongLoginException, WrongPasswordException {
 
         boolean a = login.length() <= 20;
-          System.out.println(a);
-        for (int i = 0; i < login.length(); i++) {
-            if (login.length() >= 20) {
-                throw new WrongLoginException();
-            }
-            if (!login.matches("[a-zA-Z0-9_]+")) {
-                throw new WrongLoginException();
-            }
-            return true;
+        System.out.println(a);
+        if (login.length() >= 20) {
+            throw new WrongLoginException();
         }
-            boolean b = password.length() < 20;
-            System.out.println(b);
-            for (int i = 0; i < password.length(); i++) {
-                if (password.length() > 20) {
-                    throw new WrongPasswordException();
-                }
-                if (!password.matches("[a-zA-Z0-9_]+")) {
-                    throw new WrongPasswordException();
-                }
-                return true;
-
-            }
-            boolean c = confirmPassword.length() == password.length();
-            System.out.println(c);
-            for (int i = 0; i < confirmPassword.length(); i++) {
-                if (confirmPassword.length() != password.length()) {
-                    throw new WrongPasswordException();
-                }
-            }
-         return true;
+        boolean d = login.matches("[a-zA-Z0-9_.]+");
+        System.out.println(d);
+        if (!login.matches("[a-zA-Z0-9_.]+")) {
+            throw new WrongEnterException();
         }
 
-    public static void main(String[] args){
+        boolean b = password.length() < 20;
+        System.out.println(b);
+        if (password.length() > 20) {
+            throw new WrongLenghtException();
+        }
+        boolean p = password.matches("[a-zA-Z0-9_.]+");
+        System.out.println(p);
+        if (!password.matches("[a-zA-Z0-9_.]+")) {
+            throw new WrongEnterException();
+        }
+
+        boolean c = confirmPassword.length() == password.length();
+        System.out.println(c);
+        if (confirmPassword.length() != password.length()) {
+            throw new WrongPasswordException();
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
         try {
             check("java_skypro.go", "D_1hWiKjjP_9", "D_1hWiKjjP_9");
         } catch (WrongLoginException e) {
             System.out.println("Логин слишком длинный. Максимальная длина логина 20 символов");
-            System.out.println("Логин содержит недопустимые символы");
-        } catch (WrongPasswordException e) {
-            System.out.println("Пароль содержит недопустимые символы");
+        }catch (WrongEnterException e){
+            System.out.println("Введен недопустимый символ");
+        } catch (WrongLenghtException e) {
+            System.out.println("Пароль слишком длинный. Максимальная длина 20 символов");
+        } catch (WrongPasswordException e){
             System.out.println("Пароли не совпадают");
         }
     }
